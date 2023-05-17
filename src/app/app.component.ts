994c6c4d9510b9983582a1e2166f9a79642f31cc
Kovid Sehgal <kovid.sehgal@bcldb.ca>
2023-05-17 14:09:04 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,23 @@
 import { Component } from '@angular/core';
+import costData from "./cost.json";  
+
+interface Cost {  
+  sku: String;  
+  effectiveDate: String;  
+  primeCostPerCase: Number;  
+  foreignPrimeCostPerCase: Number;  
+  currencyLabel: String;  
+  currencyRate: Number;  
+  currencyEffectiveDate: String;  
+  dutyPaidCostPerCase: Number;  
+  domesticChargesPerCase: Number; 
+  estimatedPricePerSU: Number;  
+  costOnly: String;  
+  basisNumber: String;  
+  comment: String;
+  externalReferenceID: String;
+  lastUpdatedBy: String
+}  
 
 @Component({
   selector: 'app-root',
@@ -7,4 +26,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-project';
+  costs: Cost[] = costData;  
 }
